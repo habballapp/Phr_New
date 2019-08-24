cb34d8aa57937391c349d2aaec215566ac486780
Shahood Hussain Mac <shahoodhussain77@gmail.com>
2019-08-24 00:45:43 -0700

    added firebase ios

--- android/app/src/main/java/com/medproject/MainApplication.java
+++ android/app/src/main/java/com/medproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.medproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
             new ReanimatedPackage(),
